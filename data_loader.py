88f79cd04e76c9117e490417e6492066f84cdfb0
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2025-03-05 10:36:40 +0100

    fix data loader

--- data_loader.py
+++ data_loader.py
@@ -5,10 +5,11 @@ import torch
 from torch.utils.data import DataLoader, random_split
 from environment import Environment
 import pdb
+from torch_geometric.data import Data
 
 def prepare_dictionary(graph):
     """
-    Convert the graph into an adjacency dictionary
+    Convert the graph into an adjacency dictionary.
     
     For each node, store:
       - type: Node type as a string ('Nest', 'Food', or 'Distractor')
@@ -19,7 +20,6 @@ def prepare_dictionary(graph):
     for node in graph.nodes():
         node_type = graph.nodes[node].get("type", "Distractor")
         neighbors = []
-
         for neighbor in graph.neighbors(node):
             edge_data = graph[node][neighbor]
             attr = {
@@ -29,40 +29,57 @@ def prepare_dictionary(graph):
             neighbors.append((neighbor, attr))
         adj_dict[node] = {"type": node_type, "neighbors": neighbors}
     return adj_dict
+
+def convert_graph_dict_to_tensors(graph_dict):
+    """
+    Converts the graph dictionary into tensors.
     
-def convert_dict_to_tensors(adj_dict, current_node_id=0):
+    Returns:
+      - x: Node feature tensor of shape [num_nodes, feature_dim]
+      - edge_index: Tensor of shape [2, num_edges]
+      - edge_attr: Tensor of shape [num_edges, 2] (distance and direction)
+    """
     type_mapping = {'Nest': [1, 0, 0], 'Food': [0, 1, 0], 'Distractor': [0, 0, 1]}
     direction_mapping = {"N": 0, "NE": 1, "E": 2, "SE": 3, "S": 4, "SW": 5, "W": 6, "NW": 7}
     
-    node_type = adj_dict["type"][0] if isinstance(adj_dict["type"], list) else adj_dict["type"]
-    features = [type_mapping.get(node_type, [0, 0, 1])]
+    nodes = sorted(graph_dict.keys())
+    features = []
+    for node in nodes:
+        node_data = graph_dict[node]
+        node_type = node_data["type"][0] if isinstance(node_data["type"], list) else node_data["type"]
+        features.append(type_mapping.get(node_type, [0, 0, 1]))
     
     edge_index = [[], []]
     edge_attrs = []
-    
-    for neighbor in adj_dict.get("neighbors", []):
-        neighbor_id_tensor, attr = neighbor
-        
-        if isinstance(neighbor_id_tensor, torch.Tensor):
-            neighbor_id = int(neighbor_id_tensor.item())
-        else:
-            neighbor_id = int(neighbor_id_tensor)
-        edge_index[0].append(current_node_id)
-        edge_index[1].append(neighbor_id)
-        
-        if torch.is_tensor(attr["distance"]):
-            distance = attr["distance"][0].item() if attr["distance"].dim() > 0 else attr["distance"].item()
-        else:
-            distance = attr["distance"]
-        direction_str = attr["direction"][0] if isinstance(attr["direction"], list) else attr["direction"]
-        direction = direction_mapping.get(direction_str, 0)
-        edge_attrs.append([distance, direction])
+    for node in nodes:
+        node_data = graph_dict[node]
+        for neighbor in node_data.get("neighbors", []):
+            neighbor_id, attr = neighbor
+            if isinstance(neighbor_id, torch.Tensor):
+                neighbor_id = int(neighbor_id.item())
+            else:
+                neighbor_id = int(neighbor_id)
+            edge_index[0].append(node)
+            edge_index[1].append(neighbor_id)
+            
+            if torch.is_tensor(attr["distance"]):
+                if attr["distance"].dim() > 0:
+                    distance = attr["distance"][0].item()
+                else:
+                    distance = attr["distance"].item()
+            else:
+                distance = attr["distance"]
+            
+            direction_str = attr["direction"][0] if isinstance(attr["direction"], list) else attr["direction"]
+            direction = direction_mapping.get(direction_str, 0)
+            edge_attrs.append([distance, direction])
     
     x = torch.tensor(features, dtype=torch.float)
     edge_index = torch.tensor(edge_index, dtype=torch.long)
     edge_attr = torch.tensor(edge_attrs, dtype=torch.float) if edge_attrs else None
-    return x, edge_index, edge_attr
-
+    
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+    return data
 
 def create_dataset(num_graphs=10, num_nodes=6, edge_connection_prob=0.3, max_tries=100, plot=False):
     dataset = []
@@ -75,6 +92,9 @@ def create_dataset(num_graphs=10, num_nodes=6, edge_connection_prob=0.3, max_tri
         dataset.append(adj_dict)
     return dataset
 
+def custom_collate_fn(batch):
+    return batch[0]
+
 if __name__ == '__main__':
     full_dataset = create_dataset(num_graphs=50, num_nodes=6, edge_connection_prob=0.3, max_tries=100, plot=False)
     
@@ -82,17 +102,13 @@ if __name__ == '__main__':
     validation_size = 10
     train_dataset, validation_dataset = random_split(full_dataset, [train_size, validation_size])
     
-    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-    validation_loader = DataLoader(validation_dataset, batch_size=1, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, collate_fn=custom_collate_fn)
+    validation_loader = DataLoader(validation_dataset, batch_size=1, shuffle=False, collate_fn=custom_collate_fn)
     
-    # for batch in train_loader:
-    #     print(batch)
-    #     break
     for batch in train_loader:
-        dictionary = batch[0]
-        node_t, edge_i, edge_atr = convert_dict_to_tensors(dictionary)
+        graph_data = convert_graph_dict_to_tensors(batch)
         
-        print("Feature tensor x:", node_t) # node type
-        print("Edge index:", edge_i) # tells the connection
-        print("Edge attributes:", edge_atr) # [distance, direction] of each connection
+        print("Feature tensor x:", graph_data.x)      # node type
+        print("Edge index:", graph_data.edge_index)     # graph connectivity
+        print("Edge attributes:", graph_data.edge_attr)   # [distance, direction] for each edge
         break
