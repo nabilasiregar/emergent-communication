3ed34dfe597c6a5a4c62a854db3837e46a85a74f
nabilasiregar <52696885+nabilasiregar@users.noreply.github.com>
2025-03-05 09:22:10 +0100

    modify dataset preparation

--- data_loader.py
+++ data_loader.py
@@ -1,82 +1,54 @@
-import torch
-from torch_geometric.data import Data
-from torch.utils.data import random_split
-from torch_geometric.loader import DataLoader
+import math
+import random
 import networkx as nx
+import torch
+from torch.utils.data import DataLoader, random_split
 from environment import Environment
 
-def graph_to_data(graph):
+def prepare_dictionary(graph):
     """
-    Convert a NetworkX graph from Environment to a PyG Data object
+    Convert the graph into an adjacency dictionary
     
-    Node Features:
-      - [type_one_hot(3), x_position, y_position]
-        => total of 5 features per node
-    
-    Edge Index: 
-      - undirected, so each edge (u, v) is stored twice (u->v and v->u)
-
-    Edge Attributes:
-      - None (we do not store distance/direction on edges, so agent
-        can compute them itself)
+    For each node, store:
+      - type: Node type as a string ('Nest', 'Food', or 'Distractor')
+      - neighbors: A list of tuples (neighbor_id, edge_attr) where edge_attr is a dict
+                   containing "distance" and "direction"
     """
-    num_nodes = graph.number_of_nodes()
-    
-    type_mapping = {
-        'Nest':        [1, 0, 0],
-        'Food':        [0, 1, 0],
-        'Distractor':  [0, 0, 1]
-    }
-    
-    node_features = []
-    for node_id in range(num_nodes):
-        node_data = graph.nodes[node_id]
-        node_type = node_data.get('type', 'Distractor')
-        type_feat = type_mapping[node_type]
-        
-        position = node_data.get('position', (0, 0))
-        x_pos, y_pos = float(position[0]), float(position[1])
-        
-        node_feat = type_feat + [x_pos, y_pos]
-        node_features.append(node_feat)
-    
-    x = torch.tensor(node_features, dtype=torch.float)
-    
-    src_nodes = []
-    dst_nodes = []
-    for u, v in graph.edges():
-        src_nodes.append(u)
-        dst_nodes.append(v)
-        src_nodes.append(v)
-        dst_nodes.append(u)
-    
-    # shape: [2, num_edges*2]
-    edge_index = torch.tensor([src_nodes, dst_nodes], dtype=torch.long)
-    
-    edge_attr = None
-    
-    return Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+    adj_dict = {}
+    for node in graph.nodes():
+        node_type = graph.nodes[node].get("type", "Distractor")
+        neighbors = []
 
-def create_dataset(num_graphs=10, num_distractors=4, connect_threshold=50.0):
+        for neighbor in graph.neighbors(node):
+            edge_data = graph[node][neighbor]
+            attr = {
+                "distance": edge_data.get("distance"),
+                "direction": edge_data.get("direction")
+            }
+            neighbors.append((neighbor, attr))
+        adj_dict[node] = {"type": node_type, "neighbors": neighbors}
+    return adj_dict
+
+def create_dataset(num_graphs=10, num_nodes=6, edge_connection_prob=0.3, max_tries=100, plot=False):
     dataset = []
-    for i in range(num_graphs):
-        env = Environment(num_distractors=num_distractors,
-                          connect_threshold=connect_threshold,
-                          plot=False)
-        network = env.graph
-        data = graph_to_data(network)
-        dataset.append(data)
+    for _ in range(num_graphs):
+        env = Environment(num_nodes=num_nodes, 
+                          edge_connection_prob=edge_connection_prob,
+                          max_tries=max_tries,
+                          plot=plot)
+        adj_dict = prepare_dictionary(env.graph)
+        dataset.append(adj_dict)
     return dataset
 
 if __name__ == '__main__':
-    full_dataset = create_dataset(num_graphs=50, num_distractors=3)
+    full_dataset = create_dataset(num_graphs=50, num_nodes=6, edge_connection_prob=0.3, max_tries=100, plot=False)
     
     train_size = 40
     validation_size = 10
     train_dataset, validation_dataset = random_split(full_dataset, [train_size, validation_size])
     
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-    validation_loader   = DataLoader(validation_dataset, batch_size=1, shuffle=False)
+    validation_loader = DataLoader(validation_dataset, batch_size=1, shuffle=False)
     
     for batch in train_loader:
         print(batch)
